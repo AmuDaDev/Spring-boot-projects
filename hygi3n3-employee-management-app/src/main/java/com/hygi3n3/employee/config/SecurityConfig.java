075d0d81857cd6f4cd50f6203e5c95ce925cebee
Amu <Amukelani.Nkombyani@fnb.co.za>
2022-06-16 15:44:10 +0200

    added implementation to use MYSQL and not H2 in-memory DB

--- hygi3n3-employee-management-app/src/main/java/com/hygi3n3/employee/config/SecurityConfig.java
+++ hygi3n3-employee-management-app/src/main/java/com/hygi3n3/employee/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.hygi3n3.employee.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,26 +10,24 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.User.UserBuilder;
 
+import javax.sql.DataSource;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	@Qualifier("securityDataSource")
+	private DataSource securityDataSource;
+
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
-		// add our users for in memory authentication
-		
-		UserBuilder users = User.withDefaultPasswordEncoder();
-		
-		auth.inMemoryAuthentication()
-			.withUser(users.username("john").password("test123").roles("EMPLOYEE"))
-			.withUser(users.username("mary").password("test123").roles("EMPLOYEE", "MANAGER"))
-			.withUser(users.username("susan").password("test123").roles("EMPLOYEE", "ADMIN"));
+		// use jdbc authentication ... oh yeah!!!
+		auth.jdbcAuthentication().dataSource(securityDataSource);
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-
 		http.authorizeRequests()
 			.antMatchers("/employees/showForm*").hasAnyRole("MANAGER", "ADMIN")
 			.antMatchers("/employees/save*").hasAnyRole("MANAGER", "ADMIN")
@@ -46,7 +46,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.exceptionHandling().accessDeniedPage("/access-denied");
 		http.csrf().disable(); //Disable CSRF protection for H2
 		http.headers().frameOptions().disable(); //Disable X-Frame-Options for H2
-		
 	}
 		
 }
